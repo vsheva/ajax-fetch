87adff7274340d47ac1e0ffdb830b2e8bd83c72c
vsheva <valera.sheva@gmail.com>
2021-11-28 18:41:57 +0200

    Refactoring with fetch

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 const data = {};
 
-const getData = fetch("db.json");
-console.log(getData);
-getData
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const getData = (url) => {
+    return fetch(url).then((response) => response.json());
+};
 
-const sendData = fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify(data),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-});
-console.log(sendData);
-sendData
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const sendData = ({url, data = {}, method = "GET"}) => {
+    return fetch(url, {
+        method: method,
+        body: data,
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    }).then((response) => response.json());
+};
+
+getData("db.json")
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+sendData({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    data: JSON.stringify(data),
+    method: "POST"
+})
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
