2fa568ec5bb6e6a67b18bb11afbf67e6ced71b94
vsheva <valera.sheva@gmail.com>
2021-12-01 21:44:02 +0200

    Fetch

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-const data = {};
-
 const getData = (url) => {
-    return fetch(url).then((response) => response.json());
+  return fetch(url)
+      .then((response) => response.json());
 };
 
-const sendData = ({url, data = {}, method = "GET"}) => {
-    return fetch(url, {
-        method: method,
-        body: data,
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    }).then((response) => response.json());
+
+const sendData = ({ url, data = {}, method = "GET" }) => {
+  return fetch(url, {
+    method: method,
+    body: data,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => response.json());
 };
+
 
 getData("db.json")
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-sendData({
-    url: "https://jsonplaceholder.typicode.com/posts",
-    data: JSON.stringify(data),
-    method: "POST"
-})
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
+    .then((response) => {
+      sendData({
+        url: "https://jsonplaceholder.typicode.com/posts",
+        data: JSON.stringify(response),
+        method: "POST",
+      }).then((data) => {
+            console.log(data);
+          })
+    }).catch((error) => {
+      console.log(error);
     });
